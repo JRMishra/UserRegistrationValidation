0f7cf7c72eec9789436dc473f4e5db57a1e4015f
JR Mishra <jrmnitdgp@gmail.com>
2020-10-11 01:29:06 +0530

    [JR Mishra]Test. Last Name Validation tests passed

--- UserValidationTesting/ValidationTesting.cs
+++ UserValidationTesting/ValidationTesting.cs
@@ -40,5 +40,37 @@ namespace UserValidationTesting
             Assert.AreNotEqual(expected, actual);
         }
 
+        [TestMethod]
+        [DataRow("Mishra")]
+        [DataRow("MISHRA")]
+        public void TestLastNameValidation_ValidNames(string lName)
+        {
+            //Arrange
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyLastName(lName);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("Mi")]
+        [DataRow("mishra")]
+        [DataRow("Mis#ra")]
+        public void TestLastNameValidation_InvalidNames(string lName)
+        {
+            //Arrange
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyLastName(lName);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
     }
 }
